e1c27691bfdc695be4920972fa0945ad73fa185c
rory preddy <roryp@bbd.co.za>
2018-02-16 10:40:50 +0200

    skeletons

---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,21 @@
+package demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/demo/ApplicationTests.java
@@ -0,0 +1,51 @@
+package demo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTests {
+
+    @Autowired
+    WebApplicationContext spring;
+    MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        //	Setup Spring MVC Test Framework mock object for out-of-container testing.
+        mockMvc = MockMvcBuilders.webAppContextSetup(spring).build();
+    }
+
+    @Test
+    public void teamsRetrieve() throws Exception {
+
+    }
+
+    @Test
+    public void teamRetrieve() throws Exception {
+
+    }
+
+    @Test
+    public void playerRetrieve() throws Exception {
+
+	
+    }
+
+}
